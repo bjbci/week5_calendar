182a2c417d870b35efc1177d026307cb48ab17b0
Brian Bulowicz <bajebi.bb@gmail.com>
2022-10-03 17:57:48 -0500

    third commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var momentTime= moment().format("dddd MMM Do");
+//console.log(moment())
+console.log(momentTime)
+
+var currentDay=document.getElementById("currentDay")
+
+console.log(currentDay)
+
+currentDay.textContent=momentTime
+
+//$('<h3>').text(moment).appendTo('currentDay')
+//console.log(h3)
+
+// var h3= document.createElement('h3')
+// h3.textContent=momentTime
+// console.log(h3)
+// console.log(momentTime)
+// document.body.appendChild[2](h3)
