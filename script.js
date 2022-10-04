cc897009aad597fc42159bd4543948412b0524ec
Brian Bulowicz <bajebi.bb@gmail.com>
2022-10-03 23:21:20 -0500

    almost midnight

--- script.js
+++ script.js
@@ -4,9 +4,74 @@ console.log(momentTime)
 
 var currentDay=document.getElementById("currentDay")
 
+var currentTime = new Date();
+currentTime.getHours();
+
+console.log(currentTime)
 console.log(currentDay)
 
 currentDay.textContent=momentTime
+
+if (currentTime<10){
+    button09.
+}else if (currentTime<11){
+    button10.
+}else if (currentTime<12){
+    button11.
+}else if (currentTime<13){
+    button12.
+}else if (currentTime<14){
+    button13.
+}else if (currentTime<15){
+    button14.
+}else if (currentTime<16){
+    button15.
+}else if (currentTime<17){
+    button16.
+}else  (currentTime<18){
+    button17.
+}
+
+if (currentTime==10){
+    button09.
+}else if (currentTime==11){
+    button10.
+}else if (currentTime==12){
+    button11.
+}else if (currentTime==3){
+    button12.
+}else if (currentTime==14){
+    button13.
+}else if (currentTime==15){
+    button14.
+}else if (currentTime==6){
+    button15.
+}else if (currentTime==17){
+    button16.
+}else  (currentTime==18){
+    button17.
+}
+
+if (currentTime>10){
+    button09.
+}else if (currentTime>11){
+    button10.
+}else if (currentTime>12){
+    button11.
+}else if (currentTime>13){
+    button12.
+}else if (currentTime>14){
+    button13.
+}else if (currentTime>15){
+    button14.
+}else if (currentTime>16){
+    button15.
+}else if (currentTime>17){
+    button16.
+}else  (currentTime>8){
+    button17.
+}
+
 
 //$('<h3>').text(moment).appendTo('currentDay')
 //console.log(h3)
